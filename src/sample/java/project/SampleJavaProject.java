91c9d2a66b33ba153b0094e5578c21f61f5f861e
hamid <hamid@example.com>
2022-03-14 12:36:48 -0700

    first commit this the first github commit

--- src/sample/java/project/SampleJavaProject.java
+++ src/sample/java/project/SampleJavaProject.java
@@ -60,7 +60,7 @@ public class SampleJavaProject implements Runnable {
      * Print the "Hello, world!" string.
      */
     public final void sayHello() {
-        System.out.printf("Hello, %s!%n", name);
+        System.out.printf("Hellooooooo World, %s!%n", name);
     }
 
     @Override
